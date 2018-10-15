f691d3483b4b255a5eb6dd7a3bde451269e757c2
KraljSersa <david.kralj.s@gmail.com>
2018-10-15 12:55:31 +0200

    Playground win data is now collected and bot name data is no longer collected.

--- internal/playground.go
+++ internal/playground.go
@@ -5,6 +5,10 @@ import (
 	"path/filepath"
 	"strconv"
 	"os"
+	"github.com/liagame/lia-SDK/x_vendor"
+	"io/ioutil"
+	"bytes"
+	"github.com/liagame/lia-SDK/internal/analytics"
 )
 
 func Playground(playgroundNumber int, botDir string, debug bool, viewReplay bool, replayViewerWidth string) {
@@ -22,6 +26,19 @@ func Playground(playgroundNumber int, botDir string, debug bool, viewReplay bool
 
 	playgroundBotDir := filepath.Join("data", "playgrounds", strconv.Itoa(playgroundNumber), "bot")
 	GenerateGame(botDir, playgroundBotDir, gameFlags)
+
+	// Reads the replay file
+	in, _ := ioutil.ReadFile(gameFlags.ReplayPath)
+
+	// Parses the replay file and outputs replayData
+	replayData, _ := x_vendor.GetReplayData(bytes.NewReader(in))
+
+	//If the user wins send data to google analytics
+	if replayData.GamerWinner == x_vendor.BOT_1 {
+		analytics.Log("playground", "win", map[string]string{
+			"playgroundNumber" : strconv.Itoa(playgroundNumber),
+		})
+	}
 
 	if viewReplay {
 		ShowReplayViewer(gameFlags.ReplayPath, replayViewerWidth)

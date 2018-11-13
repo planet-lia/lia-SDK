f89c01d0fa0dc5918dcc5443b489a34114f63b4a
svigelj <aljaz.svigelj@gmail.com>
2018-11-13 06:59:39 +0100

    Removed slow first run output

--- internal/compile.go
+++ internal/compile.go
@@ -20,7 +20,6 @@ func Compile(botDir string) {
 
 	// Prepare bot
 	fmt.Println("Preparing bot...")
-	fmt.Println("The first run is a bit slower, later runs will be much faster.")
 	if err := prepareBot(botDirAbsPath, lang); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to run prepare bot script for bot %s and lang %s. %s\n", botDirAbsPath, lang.Name, err)
 		os.Exit(lia_SDK.PreparingBotFailed)
